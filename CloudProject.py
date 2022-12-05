0d79a9cd9ea3225fb7899725e589b17d70136dba
Intae0099 <81225723+Intae0099@users.noreply.github.com>
2022-12-05 22:05:29 +0900

    Create CloudProject.py
    init menu

---
+++ CloudProject.py
@@ -0,0 +1,49 @@
+import boto3
+
+def menu():
+    print("------------------------------------------------------------")
+    print("1. list instance \t2. available zones")
+    print("3. start instance \t4. available regions")
+    print("5. stop instance \t6. create instance")
+    print("7. reboot instance \t8. list images")
+    print("9. credits instance 10. list tags")
+    print("\t\t\t\t\t99. quit")
+    print("------------------------------------------------------------")
+    menu_num = input("Enter an integer : ")
+    return menu_num
+  
+ACCESS_KEY = input("AWS_ACCESS_KEY_ID : ")
+SECRET_KEY = input("AWS_SECRET_ACCESS_KEY_ID : ")
+
+ec2_client = boto3.client('ec2', region_name='ap-northeast-1', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+ec2_resource = boto3.resource('ec2', region_name='ap-northeast-1', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+ec2_session = boto3.Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+  
+  while(1):
+    menu_string = menu()
+    menu_num = int(menu_string)
+    if menu_num == 1:
+        list_ins(ec2_resource)
+    if menu_num == 2:
+        avail_zone(ec2_client)
+    if menu_num == 3:
+        start_ins(ec2_client)
+    if menu_num == 4:
+        avail_region(ec2_client)
+    if menu_num == 5:
+        stop_ins(ec2_client)
+    if menu_num == 6:
+        create_ins(ec2_client)
+    if menu_num == 7:
+        reboot_ins(ec2_client)
+    if menu_num == 8:
+        list_img(ec2_client)
+    if menu_num == 9:
+        ins_credit(ec2_client)
+    if menu_num == 10:
+        tag_list(ec2_client)
+    if menu_num == 99:
+        print("Quit")
+        break
+    elif menu_num is not None:
+        print("\nSelect again")
